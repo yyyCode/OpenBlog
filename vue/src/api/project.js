76f881a22fb56aa214eddb2b1b3fcb5bdffc7e7e
yqz <2678785492@qq.com>
2026-07-22 01:12:45 +0800

    fix(project): use ESM import for API_BASE in coverUrl

--- vue/src/api/project.js
+++ vue/src/api/project.js
@@ -1,4 +1,4 @@
-import { request } from './http'
+import { API_BASE, request } from './http'
 
 export function fetchProjects(page = 0, size = 20) {
   return request(`/api/v1/projects?page=${page}&size=${size}`)
@@ -33,6 +33,5 @@ export function deleteProject(id) {
 
 export function coverUrl(key) {
   if (!key) return ''
-  const { API_BASE } = require('./http')
   return `${API_BASE}/api/v1/media/files/${key}`
 }
