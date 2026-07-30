3656ac2dd854d04aebcb641291c9b862ba0927fa
yqz <2678785492@qq.com>
2026-07-31 00:26:41 +0800

    style(header): rename to 微码平台, enlarge logo and text in top bar

--- vue/src/components/BlogHeader.vue
+++ vue/src/components/BlogHeader.vue
@@ -3,7 +3,7 @@
     <div class="site-top-bar-inner">
       <router-link to="/" class="site-brand" aria-label="返回首页">
         <img class="site-brand-logo" src="/site-icon.png" alt="" aria-hidden="true" />
-        <span class="site-brand-text">{{ (siteConfig && siteConfig.blog_name) || '烧仙草冰室' }}</span>
+        <span class="site-brand-text">{{ (siteConfig && siteConfig.blog_name) || '微码平台' }}</span>
       </router-link>
 
       <div class="site-search-bar" aria-label="搜索">
